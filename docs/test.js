61eda29bb94bc79f4bc1bbcad134b31034fed60b
juridon <66515042+juridon@users.noreply.github.com>
2022-08-24 11:47:12 +0900

    Create test.js

---
+++ docs/test.js
@@ -0,0 +1,9 @@
+(function() {
+  'use strict';
+  const events1 = ['app.record.create.show'];
+  kintone.events.on(events1, (event) => {
+    const record = event.record;
+    console.log("test");
+  });
+  
+})();
