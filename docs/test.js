b521ed7ed00023757e11ef7a191365a3ca726161
juridon <66515042+juridon@users.noreply.github.com>
2022-08-24 11:50:10 +0900

    Update test.js

--- docs/test.js
+++ docs/test.js
@@ -3,7 +3,7 @@
   const events1 = ['app.record.create.show'];
   kintone.events.on(events1, (event) => {
     const record = event.record;
-    console.log("test");
+    console.log(record);
   });
   
 })();
