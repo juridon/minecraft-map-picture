03970fadd7a2a43b5b5efbc7df0dd4dd6ee69123
juridon <66515042+juridon@users.noreply.github.com>
2022-09-01 00:37:03 +0900

    Delete test.js

--- docs/test.js
+++
@@ -1,9 +0,0 @@
-(function() {
-  'use strict';
-  const events1 = ['app.record.create.show'];
-  kintone.events.on(events1, (event) => {
-    const record = event.record;
-    console.log(record);
-  });
-  
-})();
